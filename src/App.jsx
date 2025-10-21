ef291405c117cd52f06b3812918ecbaf9fdd34a0
vuthuongcntt <vuthongcntt.work@gmail.com>
2025-10-21 18:48:57 +0700

    Thiet ke giao dien quan tri Admin

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ import MyBookings from './pages/MyBookings'
 import Favorite from './pages/Favorite'
 import {Toaster} from 'react-hot-toast'
 import Footer from './components/Footer'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
 
 const App = () => {
 
@@ -18,7 +23,6 @@ const App = () => {
     <>
       <Toaster />
       {!isAdminRoute && <Navbar />}
-      <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/movies' element={<Movies />} />
@@ -26,6 +30,13 @@ const App = () => {
         <Route path='/movies/:id/:date' element={<SeatLayout />} />
         <Route path='/my-bookings' element={<MyBookings />} />
         <Route path='/favorite' element={<Favorite />} />
+
+        <Route path='/admin/*' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-shows" element={<AddShows />} />
+          <Route path="list-shows" element={<ListShows />} />
+          <Route path="list-bookings" element={<ListBookings />} />
+        </Route>
         
       </Routes>
       {!isAdminRoute && <Footer />}
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
     <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/src/assets/backgroundImage.png")] bg-center h-screen'>
       <img src={assets.marvelLogo} alt="" className="max-h-11 lg:h-11 mt-20"/>
 
-      <h1 className='text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110'>Guardians <br /> of the Galaxy</h1>
+      <h1 className='text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110'>Vệ binh <br /> giải ngân hà</h1>
 
       <div className='flex items-center gap-4 text-gray-300'>
         <span>Action | Adventure | Sci-Fi</span>
@@ -23,7 +23,7 @@ const HeroSection = () => {
 
       </div>
       <p className='max-w-md text-gray-300'>
-        In a post-apocalyptic world where cities ride on wheels and consume each other to survive, two people meet in London and try to stop a conspiracy.
+        Trong thế giới hậu tận thế, nơi các thành phố di chuyển trên bánh xe và tiêu diệt lẫn nhau để sinh tồn, hai người gặp nhau ở London và cố gắng ngăn chặn một âm mưu.
       </p>
       <button onClick={() => navigate('/movies')} className='flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>
         Explore Movies
